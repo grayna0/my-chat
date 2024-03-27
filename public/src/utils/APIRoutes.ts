33d7245c85e9244605e6e8e611d953642f5c079e
grayna0 <grayna0164@gmail.com>
2024-03-27 11:06:58 +0700

    merge

--- public/src/utils/APIRoutes.ts
+++ public/src/utils/APIRoutes.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export const host = "http://localhost:10000"
-=======
-export const host = "https://servcer-chatapp.vercel.app"
->>>>>>> ec5be58ebdafd80d0b6c02cd380871e59e6143c3
 
 
 export const registerRoute =`${host}/api/auth/register` 
